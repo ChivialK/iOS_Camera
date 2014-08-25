e8f76e570ae45ec44c8f024ef9d495f8a02e024f
ChivialK <chivialk9913@gmail.com>
2014-08-25 13:57:20 +0800

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by RavenC on 2014/8/25.
+//  Copyright (c) 2014年 ChivialK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
